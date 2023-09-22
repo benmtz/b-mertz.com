c78a0e4771301932a1e1fc184279094189902cd5
Ben MERTZ <benjamin.mertz@techsoft3d.com>
2023-09-22 13:40:15 +0200

    Replace most of the css with tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Root from "./routes/root.tsx";
+import Experiences from './routes/experiences.tsx';
+import Projects from './routes/Projects.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "experiences",
+        element: <Experiences />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      }
+    ]
+  }, 
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
