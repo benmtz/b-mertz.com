cde3ce74483f484a5ff1ad8e004ca8cc4fcc2a8d
Ben MERTZ <ben.mertz.dev@gmail.com>
2023-10-02 21:04:07 +0200

    feat: design init

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import './Header.css';
+import tiling from './assets/tiling_forest.png';
+
+function Header() {
+  return (
+    <>
+      <div className='header' style={{ backgroundImage: `url(${tiling})` }}>
+       aaaa
+      </div>
+    </>
+  )
+}
+
+export default Header
