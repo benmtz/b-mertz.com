8e312fda8b36dc808559676490086942cadd3e17
Ben MERTZ <benjamin.mertz@techsoft3d.com>
2023-10-08 21:59:40 +0200

    feat: cloudflare dns

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -3,6 +3,12 @@ resource "aws_cloudfront_distribution" "this" {
 
   enabled = true
 
+  aliases = ["b-mertz.com"]
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:455503544814:certificate/adf2ce32-270b-45d2-8700-279ae0e903a8"
+    ssl_support_method  = "sni-only"
+  }
   origin {
     origin_id   = local.s3_origin_id
     domain_name = local.s3_domain_name
@@ -12,12 +18,7 @@ resource "aws_cloudfront_distribution" "this" {
       https_port             = 443
       origin_protocol_policy = "http-only"
       origin_ssl_protocols   = ["TLSv1.2"]
-      acm_certificate_arn = "arn:aws:acm:eu-west-3:455503544814:certificate/ddf3560c-4ea6-4f4a-8e90-f8c8b16c1e79"
     }
-
-
-
-
   }
 
   default_cache_behavior {
@@ -46,10 +47,11 @@ resource "aws_cloudfront_distribution" "this" {
     }
   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-
   price_class = "PriceClass_200"
+
+  tags = {
+    Name        = local.s3_name
+    Environment = "Prod"
+  }
 
 }
