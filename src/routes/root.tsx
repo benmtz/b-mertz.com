027a4e23a92927665620e39319a00d84114ea6f9
Ben MERTZ <ben.mertz.dev@gmail.com>
2023-10-14 20:52:57 +0200

    feat: background on main element in order to prepare for stickiness

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,15 +3,21 @@ import Footer from "../components/Footer.tsx";
 import Header from "../components/Header.tsx";
 
 export function Root() {
+
+  // const currentTheme = "waves"
+
   return (
-    <div className='h-full w-full bg-repeat bg-mj box-content'>
-      <Header tile={"multicolor_waves"}/>
-      <main className="bg-gray-50 w-full">
-        <div className="container p-4 py-16 m-auto">
-          <Outlet />
+    <div className='relative h-full bg-repeat bg-mj box-content' style={{ backgroundImage: `url(/tiles/tiling_multicolor_waves.png)`}}>
+      <Header/>
+      <main className="relative">
+        <div className="top-0 h-2 sticky z-1 backdrop-brightness-50" />
+        <div className="bg-gray-50 sticky">
+          <div className="container p-4 py-16 m-auto">
+            <Outlet />
+          </div>
         </div>
       </main>
-      <Footer tile={'multicolor_waves'} />
+      <Footer/>
     </div>
   );
 }
